18ccc5d92555f3b6c9711fe26b763282236772b5
oladipo ojo <oladipoaojo@gmail.com>
2022-11-14 17:50:57 -0600

    Add project files.

---
+++ DevOpsDemo/Repository/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DevOpsDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOpsDemo.Repository
+{
+    interface IPostRepository
+    {
+        List<PostViewModel> GetPosts();
+    }
+}
